9479f319a9906c10e64f6ebc5a3aa2955f1ef204
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-10-14 15:46:53 +0300

    Update App and Loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,43 +14,23 @@ export const App = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    if (searchLine === '') {
+      return
+    }
+
     setIsLoading(true);
     getImages(searchLine, page).then(data => {
-      return(
-        data.hits.length === 0
-        ? Notify.info('Ops! We did not find any images matching your request. Please try again.')
-        : setImages(prevState => [...prevState, ...data.hits])
-      )
-    });
-    setIsLoading(false);
-  }, [searchLine, page,]);
-
-// async componentDidUpdate(_, prevState) {
-//   try {
-//     const {page, searchLine} = this.state;
-
-//     if (prevState.searchLine !== searchLine ||
-//         prevState.page !== page) {
-
-//       this.setState({isLoading: true});    
-//       await getImages(searchLine, page).then(data => {
-//         return(
-//           data.hits.length === 0
-//           ? Notify.info('Ops! We did not find any images matching your request. Please try again.')
-//           : this.setState(prevState => ({
-//             images: [...prevState.images, ...data.hits],
-//           }))
-//         )
-//       } 
-//       )
-//       this.setState({isLoading: false})
-//     }
-//   } catch(error){
-//       this.setState({isLoading: true}); 
-//       console.log(error)
-//       this.setState({isLoading: false})
-//   } 
-// }
+      data.hits.length === 0
+      ? Notify.info('Ops! We did not find any images matching your request. Please try again.')
+      : setImages(prevState => [...prevState, ...data.hits])
+      setIsLoading(false)
+    })
+    .catch(error => {
+      setIsLoading(true); 
+            console.log(error)
+      setIsLoading(false);
+        })
+  }, [searchLine, page]);
 
 const handleFormSubmit = (searchLine) => {
   setPage(1);
@@ -62,8 +42,7 @@ const loadMore = () => {
   setPage(prevState => prevState+1)
 };
 
-    // const {isLoading, images} = this.state;
-  return (
+return (
       <Box bg="mute" >
         {isLoading && <Loader />}
         <Searchbar onFormSubmit={handleFormSubmit}/>
@@ -74,66 +53,3 @@ const loadMore = () => {
       </Box>
     );
 };
-
-// export class App extends React.Component {
-//   state = {
-//     page: 1,
-//     searchLine: "",
-//     images: [],
-//     isLoading: false,
-// };
-
-// async componentDidUpdate(_, prevState) {
-//   try {
-//     const {page, searchLine} = this.state;
-
-//     if (prevState.searchLine !== searchLine ||
-//         prevState.page !== page) {
-
-//       this.setState({isLoading: true});    
-//       await getImages(searchLine, page).then(data => {
-//         return(
-//           data.hits.length === 0
-//           ? Notify.info('Ops! We did not find any images matching your request. Please try again.')
-//           : this.setState(prevState => ({
-//             images: [...prevState.images, ...data.hits],
-//           }))
-//         )
-//       } 
-//       )
-//       this.setState({isLoading: false})
-//     }
-//   } catch(error){
-//       this.setState({isLoading: true}); 
-//       console.log(error)
-//       this.setState({isLoading: false})
-//   } 
-// }
-
-// handleFormSubmit = (searchLine) => {
-//   this.setState({
-//     page:1,
-//     searchLine:searchLine,
-//     images: [],})
-// };
-
-// loadMore = () => {
-//   this.setState(prevState => ({
-//     page: prevState.page + 1,
-//   }));
-// };
-
-//   render() {
-//     const {isLoading, images} = this.state;
-//     return (
-//       <Box bg="mute" >
-//         {isLoading && <Loader />}
-//         <Searchbar onFormSubmit={this.handleFormSubmit}/>
-//        <main style={{ textAlign: 'center', marginTop: '20px' }}>
-//        <ImageGallery images={images}/>
-//         {images.length > 0 && <Button handleClick={this.loadMore}/>}
-//         </main>
-//       </Box>
-//     );
-//   }
-// };
